aee03ccb07831470bf5bc85a22a53d0b427c42e5
Prithwiraj Mazumdar <prithwi1016@gmail.com>
2024-02-04 21:28:21 +0530

    Python

---
+++ Q7.py
@@ -0,0 +1,7 @@
+# Create a mixed String using the following rules
+a=input("Enter String or Integer : ")
+b=input("Enter String or Integer : ")
+if(b in a):
+    print("True")
+else:
+    print("False")
