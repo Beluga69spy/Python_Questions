f79a4f6f8c0d0c61ff895a2aa8618b40a5dc7c2d
Prithwiraj Mazumdar <prithwi1016@gmail.com>
2024-02-04 12:47:06 +0530

    Python

---
+++ Q9.py
@@ -0,0 +1,13 @@
+def find(string, substring):
+    string_lower = string.lower()
+    substring_lower = substring.lower()
+    occurrence  =[]
+    for i in range(len(string_lower)):
+        if string_lower.startswith(substring_lower, i):
+            occurrence.append(i)
+    return occurrence
+string = "The quick brown fox jumps over the lazy dog"
+substring = "the"
+occurrence = find(string, substring)
+print("Occurrences of '{}' :".format(substring))
+print(occurrence)
