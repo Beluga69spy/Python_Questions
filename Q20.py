1e5d3a222411ecbfbce15cde76ef3e28fc526c37
Prithwiraj Mazumdar <prithwi1016@gmail.com>
2024-02-04 22:24:07 +0530

    main

---
+++ Q20.py
@@ -0,0 +1,7 @@
+# Print the lowest number
+numbers = [11, 54, 7, 96, 2, 33, 87, 25]
+if numbers:
+    lowest_number = min(numbers)
+    print("Lowest number:", lowest_number)
+else:
+    print("The list is empty.")
