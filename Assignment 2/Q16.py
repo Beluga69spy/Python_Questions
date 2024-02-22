a22a94d9530a94c21a7c3891d9e093c30538d623
Prithwiraj Mazumdar <prithwi1016@gmail.com>
2024-02-22 19:35:50 +0530

    Python

---
+++ Assignment 2/Q16.py
@@ -0,0 +1,10 @@
+def tup(tuple1, tuple2):
+
+  combined = set(tuple1) | set(tuple2)
+  return tuple(combined)
+tuple1 = (1, 2, 3, 4)
+tuple2 = (3, 4, 5, 6)
+
+merged = tup(tuple1, tuple2)
+
+print(f"Merged tuple without duplicates: {merged}")
